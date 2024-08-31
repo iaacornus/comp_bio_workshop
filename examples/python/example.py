9cfa52483d557b3129ab9f65a8dfdcf551707167
James Aaron Erang <iaacornus.devel@gmail.com>
2024-09-01 01:25:24 +0800

    python example

---
+++ examples/python/example.py
@@ -0,0 +1,11 @@
+def main() -> int:
+    hello: str = "hello world!"
+
+    if hello == "hello world!":
+        print("The tradition in programming is to print")
+        print(f"Hello World! Like this in Python, {hello}")
+
+    return 0
+
+if __name__ == "__main__":
+    main()
