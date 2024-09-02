51170043b5888c39d821cb94b0d733f35a99888f
James Aaron Erang <iaacornus.devel@gmail.com>
2024-09-02 11:15:28 +0800

    use void main() and for loop

--- examples/c/c_example.c
+++ examples/c/c_example.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-    /* this is comment */
-    // this is ignored
-    // this is variable
-    char hello[] = "hello world!";
+void main() { /* this is comment, this is ignored */
+    char hello[] = "hello world!"; // this is variable
 
-    // compare this variable to a string
-    if ( strcmp(hello, "hello world!") == 0 ) {
-        printf("The tradition in programming is to print\n");
-        printf("Hello World! Like this in C, %s\n", hello);
+    if (strcmp(hello, "hello world!") == 0) {
+        printf(
+            "The tradition in programming is to print\n"
+        );
+        printf(
+            "Hello World! Like this in C, %s\n", hello
+        );
+
+        for (int i = 1; i < 4; i++) { // loop over range of 1 to
+            printf(                   // 4 and print hello world!
+                "%d hello world!\n", i
+            );
+        }
     }
 
-    return 0;
+    // return 0;
 }
