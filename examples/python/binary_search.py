7d542574ca94bd84486eefcbe480ea8fdd50a40a
James Aaron Erang <iaacornus.devel@gmail.com>
2024-09-11 06:33:37 +0800

    remove H and L as property of object

--- examples/python/binary_search.py
+++ examples/python/binary_search.py
@@ -1,21 +1,19 @@
 class BinarySearch:
     """ Binary search solution in Python """
 
-    def __init__(self, arr: list[int], N: int) -> None:
+    def __init__(self, arr: list[int]) -> None:
         self.arr: list[int] = arr
-        self.H: int = N - 1 # max
-        self.L: int = 0     # min
 
     def solution(self, T: int) -> int:
         """ Solution """
 
-        while self.L <= self.H:
-            M: int = (self.H + self.L) // 2
+        while (L := 0) <= (H := (len(arr) - 1)):
+            M: int = (H + L) // 2
 
             if self.arr[M] < T:
-                self.L = M + 1
+                L = M + 1
             elif self.arr[M] > T:
-                self.H = M - 1
+                H = M - 1
             else: # if self.arr[M] == T
                 return M
 
