4ee9069670bdfecce36b48f70d3e113c89c07546
James Aaron Erang <iaacornus.devel@gmail.com>
2024-09-10 23:10:45 +0800

    comments and docstrings

--- examples/python/binary_search.py
+++ examples/python/binary_search.py
@@ -1,12 +1,14 @@
 class BinarySearch:
-    """ Binary search solution in python """
+    """ Binary search solution in Python """
 
     def __init__(self, arr: list[int], n: int) -> None:
         self.arr: list[int] = arr
-        self.H: int = n - 1
-        self.L: int = 0
+        self.H: int = N - 1 # max
+        self.L: int = 0     # min
 
     def solution(self, T: int) -> int:
+        """ Solution """
+
         while self.L <= self.H:
             M: int = (self.H + self.L) // 2
 
