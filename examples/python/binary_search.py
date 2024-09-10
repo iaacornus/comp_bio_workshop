35b1683f77f2820a2def659bd4665925952d1acb
James Aaron Erang <iaacornus.devel@gmail.com>
2024-09-10 23:10:19 +0800

    optimize the conditionals

--- examples/python/binary_search.py
+++ examples/python/binary_search.py
@@ -9,12 +9,12 @@ class BinarySearch:
     def solution(self, T: int) -> int:
         while self.L <= self.H:
             M: int = (self.H + self.L) // 2
-            if self.arr[M] == T:
-                return M
 
             if self.arr[M] < T:
                 self.L = M + 1
-            else:
+            elif self.arr[M] > T:
                 self.H = M - 1
+            else: # if self.arr[M] == T
+                return M
 
         return 1
